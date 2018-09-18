b48244ee7836df724c8cf38c7dd81ad5d4eb1116
Matt Watier <mattwatier@gmail.com>
2018-09-18 14:34:03 -0400

    Updates from Meeting

---
+++ docs/components/Reveal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="grid-container">
+    <div class="x-grid">
+
+        
+        <div id="reveal-dialog" class="reveal" data-reveal>
+            <button class="close-button" data-close aria-label="Close modal" type="button">
+              <span aria-hidden="true"><i class="fas fa-times"></i></span>
+            </button>
+            <slot name="header">
+            default header
+            </slot>
+            <slot name="content">
+              <h1>Awesome. I Have It.</h1>
+              <p class="lead">Your couch. It is mine.</p>
+              <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+          </slot>
+      </div>
+      <p class="hide"><a v-on:click="openReveal()">Click me for a modal</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reveal',
+  mounted() {
+    this.reveal = new Foundation.Reveal($('#reveal-dialog'), {
+      // These options can be declarative using the data attributes
+      animationIn: 'scale-in-up',
+    });
+  },
+  data() {
+    return {
+      msg: 'Reveal',
+    };
+  },
+  methods: {
+    // Added the below openReveal method for two reasons:
+    // 1) There was a bug preventing the reveal from working once
+    // you navigated away and back to the reveal component.
+    // 2) Most dialogs will need to be opened using code.
+    openReveal() {
+      this.reveal.open();
+    },
+  },
+  destroyed() {
+    this.reveal.destroy();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ docs/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="login ">
+	<div class="grid-x">
+		<div class="cell small-10 medium-8 large-4 margin_auto">
+			<div class="card shadow_2 cell  ">
+			<div class="card-divider color_white font-size_3 padding_4 texture_dust  back_primary">
+				ACC Staff Login
+			</div>
+			<div class="card-section">
+				<div class="grid-container">
+					<div class="grid-x grid-padding-x">
+						<div class="medium-6 cell">
+							<label>User Name
+								<input type="text" placeholder="" >
+							</label>
+						</div>
+						<div class="medium-6 cell">
+							<label>Password
+								<input type="text" placeholder="">
+							</label>
+						</div>
+					</div>
+				</div>
+				<div class="grid-container">
+					<a href="/#/badge" class="button disabled float-right color_white margin-bottom_0 border_radius">Log in <i class="fal fa-arrow-alt-right"></i></a>
+				</div>
+			</div>
+		</div>
+<div class="padding_3 font_n1 text-center">
+	<a href="/#/about" class="link secondary text_underline">About this Utlity</a>
+</div>
+		</div>
+	</div>
+</div>
+</template>
+<script>
+export default {
+name: 'login'
+}
+</script>
